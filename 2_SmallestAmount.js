88367c1270ff98d23b4fbc7f079f6c8a205bf3f2
SirSnow <hakuhal.snow@gmail.com>
2020-04-28 21:30:40 +0800

    更新第二种方法
    clone的时候直接使用slice()就行了

--- 2_SmallestAmount.js
+++ 2_SmallestAmount.js
@@ -4,61 +4,50 @@
 //例如[1,3,2]数组，需要[1,2,1]就都会满意
 
 function countSmallestAmount(array) {
-  let indexArray = [];
-  let arrayContainer = [];
-
-  //首先我要把array变成一个对象数组
-  for (let i in array) {
-    arrayContainer.push({
-      index: i,
-      value: array[i],
+  let arrayContainer = array
+    .slice()
+    .map((el, index) => {
+      return { index: index, value: el };
+    })
+    .sort(function (a, b) {
+      return a.value - b.value;
     });
-  }
 
-  arrayContainer.sort(function (a, b) {
-    return a.value - b.value;
-  });
   //排序后的数组取得相应的结构
-  indexArray = arrayContainer.map((obj) => obj.index);
-  console.log(arrayContainer);
-  console.log(indexArray);
-  let stepArray = [1];
+  let indexArray = arrayContainer.map((obj) => obj.index);
+
+  let resultArray = [1];
   function recuresion(step = 1, i = 1) {
-    if (indexArray.length == stepArray.length) return stepArray;
+    if (indexArray.length == resultArray.length) return resultArray;
 
     if (Math.abs(indexArray[i] - indexArray[i - 1]) == 1) {
       step++;
     } else {
       step = 1;
     }
-    // console.log("step", step);
-    stepArray = [...stepArray, step];
+    resultArray = [...resultArray, step];
     i++;
     return recuresion(step, i);
   }
   recuresion();
-  // console.log("配比方案", stepArray);
-  return stepArray.reduce((a, b) => a + b);
+
+  return resultArray.reduce((a, b) => a + b);
 }
 
 function countSmallestAmount2(array) {
-  let indexArray = [];
-  let arrayContainer = [];
-  for (let i in array) {
-    arrayContainer.push({
-      index: i,
-      value: array[i],
-    });
-  }
-
-  arrayContainer.sort(function (a, b) {
-    return a.value - b.value;
-  });
-
-  s = arrayContainer.map((obj) => obj.index);
-  // console.log(s);
-
   let b = new Array(10).fill(0);
+  b[0] = 1;
+  let arrayContainer = array
+    .slice()
+    .map((el, index) => {
+      return { index: index, value: el };
+    })
+    .sort(function (a, b) {
+      return a.value - b.value;
+    });
+
+  let s = arrayContainer.map((obj) => obj.index);
+  // console.log(s);
   for (let i = 0; i < s.length - 1; i++) {
     let _s = parseInt(s[i]);
     let _r = _s + 1 > 9 ? 9 : _s + 1,
@@ -69,7 +58,7 @@ function countSmallestAmount2(array) {
     }
   }
   console.log("b", b);
-  return b.reduce((a, b) => a + b) + 1;
+  return b.reduce((a, b) => a + b);
 }
 
 let array = [10, 9, 8, 7, 1, 2, 3, 4, 5, 6];
