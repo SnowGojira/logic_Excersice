a1ce5a67fd780a549ddd6444050a65aa326c0400
SirSnow <hakuhal.snow@gmail.com>
2020-05-07 19:47:42 +0800

    添加了二叉树的add方法的源码

--- computerScienceEx/8_binarySearchTree.js
+++ computerScienceEx/8_binarySearchTree.js
@@ -9,7 +9,34 @@ class Tree {
     return this.root;
   }
 
-  add(value) {}
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+      return;
+    }
+    let current = this.root;
+    while (true) {
+      if (current.value > value) {
+        //数组插入左边
+        if (current.left) {
+          //如果已经有children了
+          //那么把指针移动向下一个，继续loop
+          current = current.left;
+        } else {
+          current.left = new Node(value);
+          return;
+        }
+      } else {
+        //数组插入右边
+        if (current.right) {
+          current = current.right;
+        } else {
+          current.right = new Node(value);
+          return;
+        }
+      }
+    }
+  }
 }
 
 class Node {
