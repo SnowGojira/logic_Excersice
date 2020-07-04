12ded06fc7fce39563378fcb6416d854d1fdef05
SirSnow <hakuhal.snow@gmail.com>
2020-07-04 22:18:16 +0800

    验证BST

--- dataStructureEx/6_BinaryTree.js
+++ dataStructureEx/6_BinaryTree.js
@@ -83,7 +83,7 @@ class BinaryTree {
   //左右比较，高度比较高的+1
   height(root = this.root) {
     //边界：空树
-    if (root == null) return -1;
+    if (this._isEmpty(root)) return -1;
     //到达叶片停止，此时高度为0
     if (this._isLeaf(root)) return 0;
     return 1 + Math.max(this.height(root.left), this.height(root.right));
@@ -91,8 +91,12 @@ class BinaryTree {
 
   //最小值计算
   //查找公式 = min(left,right,root)
-  //左中右节点中的最小值。
+  //左中右节点中的最小值。O(n)
   min(root = this.root) {
+    //edge case
+    if (this._isEmpty(root))
+      throw new Error("null pointer: this tree is empty");
+
     if (this._isLeaf(root)) return root.value;
 
     let left = this.min(root.left);
@@ -100,8 +104,56 @@ class BinaryTree {
     return Math.min(Math.min(left, right), root.value);
   }
 
+  //Binary Search Tree
+  //只需要找到最左边的值
+  BSTmin(root = this.root) {
+    //edge case
+    //树为空
+    if (this._isEmpty(root))
+      throw new Error("null pointer: this tree is empty");
+
+    //最左边的叶片是最小值
+    if (this._isLeaf(root)) return root.value;
+
+    return this.min(root.left);
+  }
+
   _isLeaf(node) {
     return node.left == null && node.right == null;
+  }
+  _isEmpty(node) {
+    return node == null;
+  }
+
+  //////=========等价=========
+  equality(tree) {
+    let root1 = this.root;
+    let root2 = tree.root;
+
+    return this.equals(root1, root2);
+  }
+  equals(root1, root2) {
+    if (root1 == null && root2 == null) return true;
+
+    if (root1 !== null && root2 !== null)
+      return (
+        root1.value == root2.value &&
+        this.equals(root1.right, root2.right) &&
+        this.equals(root1.left, root2.left)
+      );
+    return false;
+  }
+
+  isBST(root = this.root, min = -Infinity, max = Infinity) {
+    console.log("root-min-max", `${root}:${min}:${max}`);
+    if (root == null) return true;
+
+    if (root.value < min || root.value > max) return false;
+
+    return (
+      this.isBST(root.left, min, root.value - 1) &&
+      this.isBST(root.right, root.value + 1, max)
+    );
   }
 }
 
@@ -114,7 +166,7 @@ class Node {
 }
 
 //=====================
-var tree = new BinaryTree();
+const tree = new BinaryTree();
 tree.insert(7);
 tree.insert(4);
 tree.insert(9);
@@ -122,8 +174,19 @@ tree.insert(1);
 tree.insert(6);
 tree.insert(8);
 tree.insert(10);
-console.log(tree);
-console.log(tree.find(10));
+
+const tree2 = new BinaryTree();
+tree2.insert(7);
+tree2.insert(4);
+tree2.insert(9);
+tree2.insert(1);
+tree2.insert(6);
+tree2.insert(8);
+tree2.insert(10);
+//console.log(tree);
+//console.log(tree.find(10));
 //tree.postOrder();
-console.log(tree.height());
-console.log(tree.min());
+console.log(tree.height() == 2);
+console.log(tree.BSTmin() == 1);
+console.log(tree.equality(tree2));
+console.log(tree.isBST());
