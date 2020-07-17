3567738c17dee2888dc8da85fd9daca40fdefe40
SirSnow <hakuhal.snow@gmail.com>
2020-07-17 20:37:29 +0800

    添加了this的全部用例

--- dataStructureEx/test.js
+++ dataStructureEx/test.js
@@ -1,10 +1,66 @@
-function sum() {
-  console.log(arguments);
-  if (Array.isArray(arguments[0])) {
-    console.log(arguments[0]);
-    return arguments[0].reduce((a, b) => a + b);
-  }
-  return [...arguments].reduce((a, b) => a + b);
+var a = "Opps Global!";
+
+function foo() {
+  console.log(this.a);
+}
+//情况一
+//foo();
+
+let obj = {
+  a: 2,
+  tags: [3, 4],
+  foo: foo,
+  baz() {
+    this.tags.forEach(function (item) {
+      this.foo();
+    });
+  },
+};
+//情况二
+//obj.foo();
+//情况三
+//obj.baz();
+
+function Obj() {
+  this.a = 2;
+  this.tags = [3, 4];
 }
 
-console.log(sum(1, 2, 3));
+Obj.prototype.foo = foo;
+Obj.prototype.baz = function () {
+  this.tags.forEach(function (item) {
+    this.foo();
+  });
+};
+
+const obj2 = new Obj();
+//情况四
+//obj2.foo();
+//obj2.baz();
+
+class OBJ {
+  constructor() {
+    this.a = 2;
+    this.tags = [3, 4];
+    this.foo = foo;
+  }
+
+  baz() {
+    this.tags.forEach(function (item) {
+      console.log(this);
+    });
+  }
+}
+
+//情况五
+const obj3 = new OBJ();
+//obj3.foo();
+obj3.baz();
+
+//情况六
+setTimeout(obj.foo, 100);
+//情况七
+const button = document.getElementById("#button");
+button.addEventListener("click", function () {
+  console.log(this);
+});
