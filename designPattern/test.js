23e41f7ebb0029d1fae104d521c9544eb59be07e
SirSnow <hakuhal.snow@gmail.com>
2020-07-18 21:12:49 +0800

    实验babel

--- designPattern/test.js
+++ designPattern/test.js
@@ -1,38 +1,5 @@
-const _storage = new WeakMap();
-const _isEmpty = new WeakMap();
-class Stack {
-  constructor() {
-    _storage.set(this, []);
-    _isEmpty.set(this, (s) => {
-      if (s.length == 0) throw new Error("the Stack is Empty");
-    });
-  }
-  get count() {
-    return _storage.get(this).length;
-  }
-  push(value) {
-    let storage = _storage.get(this);
+import { Circle } from "./module.js";
 
-    storage.push(value);
-  }
+const c = new Circle(3);
 
-  pop() {
-    let storage = _storage.get(this);
-    const isEmpty = _isEmpty.get(this);
-
-    isEmpty(storage);
-
-    return storage.pop();
-  }
-
-  peak() {
-    const storage = _storage.get(this);
-    const isEmpty = _isEmpty.get(this);
-
-    isEmpty(storage);
-
-    return storage[storage.length - 1];
-  }
-}
-
-const s = new Stack();
+console.log(c.draw());
