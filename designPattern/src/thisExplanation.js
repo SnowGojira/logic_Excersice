39dd6b3ee0b4355ff1919b534a0019710a7a14b4
SirSnow <hakuhal.snow@gmail.com>
2020-07-23 22:41:01 +0800

    添加新的规则注释

--- designPattern/src/thisExplanation.js
+++ designPattern/src/thisExplanation.js
@@ -16,10 +16,24 @@
 // foo();
 
 //规律三 对象里面的method里的 this = obj
-const obj = {
-  foo: function () {
-    console.log("this", this);
-  },
-};
 
-obj.foo();
+// const obj = {
+//   foo: function () {
+//     console.log("this", this);
+//   },
+// };
+
+// obj.foo();
+
+//////////造成混乱的根源////////////
+//容易发生混乱的情景一：函数地址
+
+//容易发生混乱的情景二：
+//2.1 回调函数
+//2.2 箭头函数
+
+//容易发生混乱的情景三：
+//3.1 ES5类
+//3.2 ES6类
+
+//容易发生混乱的情景四：addEventListener
